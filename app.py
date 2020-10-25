d449b1ab60220d5a37a8375cac26db888dadfc98
shingler <shingler@gf-app.cn>
2020-10-25 18:01:34 +0800

    更新错误码及说明

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import jsonify, request
 import datetime
 from src import create_app
 from bin import cust_vend, fa, invoice, other
+from src import error
+
 app = create_app()
 
 
@@ -29,8 +31,16 @@ def cust_vend_api():
         try:
             entry_no = cust_vend.main(company_code=company_code, api_code=api_code, cur_date=cur_date, retry=True if retry else False)
             res = {"status": 1, "entry_no": entry_no}
+        except error.DataFieldEmptyError as ex:
+            res = {"status": 10001, "error_message": str(ex)}
+        except error.InvoiceEmptyError as ex:
+            res = {"status": 10002, "error_message": str(ex)}
+        except error.DataLoadError as ex:
+            res = {"status": 10003, "error_message": str(ex)}
+        except error.DataLoadTimeOutError as ex:
+            res = {"status": 10004, "error_message": str(ex)}
         except Exception as ex:
-            res = {"status": 0, "error_message": str(ex)}
+            res = {"status": 10000, "error_message": str(ex)}
     return jsonify(res)
 
 
@@ -56,9 +66,16 @@ def fa_api():
         try:
             entry_no = fa.main(company_code=company_code, api_code=api_code, cur_date=cur_date, retry=True if retry else False)
             res = {"status": 1, "entry_no": entry_no}
+        except error.DataFieldEmptyError as ex:
+            res = {"status": 20001, "error_message": str(ex)}
+        except error.InvoiceEmptyError as ex:
+            res = {"status": 20002, "error_message": str(ex)}
+        except error.DataLoadError as ex:
+            res = {"status": 20003, "error_message": str(ex)}
+        except error.DataLoadTimeOutError as ex:
+            res = {"status": 20004, "error_message": str(ex)}
         except Exception as ex:
-            print(ex)
-            res = {"status": 0, "error_message": str(ex)}
+            res = {"status": 20000, "error_message": str(ex)}
     return jsonify(res)
 
 
@@ -84,8 +101,16 @@ def invoice_api():
         try:
             entry_no = invoice.main(company_code=company_code, api_code=api_code, cur_date=cur_date, retry=True if retry else False)
             res = {"status": 1, "entry_no": entry_no}
+        except error.DataFieldEmptyError as ex:
+            res = {"status": 30001, "error_message": str(ex)}
+        except error.InvoiceEmptyError as ex:
+            res = {"status": 30002, "error_message": str(ex)}
+        except error.DataLoadError as ex:
+            res = {"status": 30003, "error_message": str(ex)}
+        except error.DataLoadTimeOutError as ex:
+            res = {"status": 30004, "error_message": str(ex)}
         except Exception as ex:
-            res = {"status": 0, "error_message": str(ex)}
+            res = {"status": 30000, "error_message": str(ex)}
     return jsonify(res)
 
 
@@ -111,8 +136,16 @@ def other_api():
         try:
             entry_no = other.main(company_code=company_code, api_code=api_code, cur_date=cur_date, retry=True if retry else False)
             res = {"status": 1, "entry_no": entry_no}
+        except error.DataFieldEmptyError as ex:
+            res = {"status": 40001, "error_message": str(ex)}
+        except error.InvoiceEmptyError as ex:
+            res = {"status": 40002, "error_message": str(ex)}
+        except error.DataLoadError as ex:
+            res = {"status": 40003, "error_message": str(ex)}
+        except error.DataLoadTimeOutError as ex:
+            res = {"status": 40004, "error_message": str(ex)}
         except Exception as ex:
-            res = {"status": 0, "error_message": str(ex)}
+            res = {"status": 40000, "error_message": str(ex)}
     return jsonify(res)
 
 
