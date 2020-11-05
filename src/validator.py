cad79c64429df1c220db5f63fb5fbb80ae5fb573
shingler <singlerwong@qq.com>
2020-11-05 17:11:03 +0800

    长度计算

--- src/validator.py
+++ src/validator.py
@@ -4,24 +4,33 @@
 
 
 class DMSInterfaceInfo:
-    chn_leng = {
+    _chn_leng = {
         "DMSTitle": 50,
         "CompanyTitle": 50
     }
 
     @classmethod
     def check_chn_length(cls, key, value):
-        if key not in cls.chn_leng:
+        if key not in cls._chn_leng:
             return True
-        elif len(value)/2 <= cls.chn_leng[key]:
+        elif cls.chn_length(value) <= cls._chn_leng[key]:
             # 按照旧版本的sql server的规定，一个汉字为两个字符
             return True
         else:
             return False
 
+    @classmethod
+    # 按照一个汉字为两个字符计数
+    def chn_length(cls, txt):
+        lenTxt = len(txt)
+        lenTxt_utf8 = len(txt.encode('utf-8'))
+        # utf-8一个汉字占3个字符，减去原计数就是多出来的2/3，再除以2就是增量。再加回去即可
+        size = int((lenTxt_utf8 - lenTxt) / 2 + lenTxt)
+        return size
+
 
 class CustVendInfo(DMSInterfaceInfo):
-    chn_leng = {
+    _chn_leng = {
         "Name": 50,
         "Address": 50,
         "Address_2": 50
@@ -29,25 +38,25 @@ class CustVendInfo(DMSInterfaceInfo):
 
 
 class FA(DMSInterfaceInfo):
-    chn_leng = {
+    _chn_leng = {
         "Description": 30,
         "SerialNo": 30
     }
 
 
 class InvoiceHeader(DMSInterfaceInfo):
-    chn_leng = {
+    _chn_leng = {
         "Description": 100
     }
 
 
 class InvoiceLine(DMSInterfaceInfo):
-    chn_leng = {
+    _chn_leng = {
         "Description": 100
     }
 
 
 class Other(DMSInterfaceInfo):
-    chn_leng = {
+    _chn_leng = {
         "Description": 100
     }
--- tests/testValidate.py
+++ tests/testValidate.py
@@ -1,2 +1,82 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+# 测试数据校验
+import pytest
+import xmltodict
+from src import validator
+
+xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<Transaction>
+    <General>
+        <DMSCode>7000320</DMSCode>
+        <DMSTitle>中国太平洋财产保险股份有限公司宁波分公司</DMSTitle>
+        <CompanyCode>7000320</CompanyCode>
+        <CompanyTitle>中国太平洋财产保险股份有限公司宁波分公司|中国太平洋财产保险股份有限公司宁波分公司</CompanyTitle>
+        <CreateDateTime>2012-07-03T14:48:36.927Z</CreateDateTime>
+        <Creator>sa</Creator>
+    </General>
+    <CustVendInfo>
+        <Type>Customer</Type>
+        <No>835194</No>
+        <Name>中国太平洋财产保险股份有限公司宁波分公司|中国太平洋财产保险股份有限公司宁波分公司</Name>
+        <Address></Address>
+        <Address2></Address2>		
+        <PhoneNo>1234567</PhoneNo>
+        <FaxNo>1234567</FaxNo>
+        <Blocked></Blocked>
+        <Email></Email>
+        <Postcode></Postcode>
+        <City>杭州市</City>
+        <Country>CN-0086</Country>
+        <Currency>RMB</Currency>
+        <ARAPAccountNo>112201</ARAPAccountNo>
+        <PricesIncludingVAT>true</PricesIncludingVAT>
+        <ApplicationMethod></ApplicationMethod>
+        <PaymentTermsCode></PaymentTermsCode>
+        <PaymentMethodCode></PaymentMethodCode>
+        <CostCenterCode></CostCenterCode>
+        <Template></Template>
+        <ICPartnerCode></ICPartnerCode>
+    </CustVendInfo>
+    <CustVendInfo>
+        <Type>Vendor</Type>
+        <No>V00000002</No>
+        <Name>XXXX汽车贸易有限公司</Name>
+        <Address></Address>
+        <Address2></Address2>
+        <PhoneNo>1234567</PhoneNo>
+        <FaxNo>1234567</FaxNo>
+        <Blocked></Blocked>
+        <Email></Email>
+        <Postcode></Postcode>
+        <City>北京市</City>
+        <Country>CN-0086</Country>
+        <Currency>RMB</Currency>
+        <ARAPAccountNo>11230102</ARAPAccountNo>
+        <PricesIncludingVAT>true</PricesIncludingVAT>
+        <ApplicationMethod></ApplicationMethod>
+        <PaymentTermsCode></PaymentTermsCode>
+        <PaymentMethodCode></PaymentMethodCode>
+        <CostCenterCode></CostCenterCode>
+        <Template></Template>
+        <ICPartnerCode></ICPartnerCode>
+    </CustVendInfo>
+</Transaction>
+'''
+
+
+def test_valid():
+    data_dict = xmltodict.parse(xml)
+    print(type(data_dict["Transaction"]["General"]))
+    print(data_dict["Transaction"]["General"])
+    for k, v in data_dict["Transaction"]["General"].items():
+        is_valid = validator.DMSInterfaceInfo.check_chn_length(k, v)
+        if k == "DMSTitle":
+            assert is_valid == True
+        elif k == "CompanyTitle":
+            assert is_valid == False
+
+
+def test_chn_length():
+    txt = "你好啊,123"
+    assert validator.DMSInterfaceInfo.chn_length(txt) == 10
