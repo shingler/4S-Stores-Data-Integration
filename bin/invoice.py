d1bdb9f19c5c03cbc43aa9ee6099977bfa614cb5
shingler <singlerwong@qq.com>
2020-11-19 18:21:39 +0800

    动态写nav迁入脚本，待测试

--- bin/invoice.py
+++ bin/invoice.py
@@ -2,6 +2,9 @@
 # -*- coding:utf-8 -*-
 import os
 import sys
+
+from src.models import navdb
+
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
@@ -21,52 +24,51 @@ from src.models.dms import Company
 def main(company_code, api_code, retry=False, file_path=None, async_ws=False):
     # 读取公司信息，创建业务对象
     company_info = db.session.query(Company).filter(Company.Code == company_code).first()
-    invoiceHeader_obj = InvoiceHeader(company_info.NAV_Company_Code, force_secondary=retry)
-    invoiceLine_obj = InvoiceLine(company_info.NAV_Company_Code)
 
-    # 保存数据到nav，需要修改数据库连接设置
-    conn_str = company_info.get_nav_connection_string(app.config)
-    app.config["SQLALCHEMY_BINDS"][
-        "%s-nav" % company_info.NAV_Company_Code] = conn_str
-
-    # 读取API设置，拿到数据
+    # 读取api设置
     api_setup = Setup.load_api_setup(company_code, api_code)
-    xml_src_path, data = invoiceHeader_obj.load_data(api_setup, file_path=file_path)
 
-    # 读取输出设置，保存General
-    general_node_dict = invoiceHeader_obj.load_api_p_out_nodes(company_code, api_code, node_type="General")
-    general_dict = invoiceHeader_obj.splice_general_info(data, node_dict=general_node_dict)
+    # 连接nav数据库
+    nav = navdb.NavDB('127.0.0.1', 'sa', 'msSqlServer2020', 'NAV', company_nav_code=company_info.NAV_Company_Code)
+    nav.prepare()
 
-    entry_no = invoiceHeader_obj.save_data_to_interfaceinfo(
-        general_data=general_dict,
-        Type=2,
-        Count=invoiceHeader_obj.get_count_from_data(data["Transaction"], "Invoice"),
-        XMLFile=xml_src_path if xml_src_path else "")
+    invh_obj = InvoiceHeader(company_code, api_code, force_secondary=retry)
+    invl_obj = InvoiceLine(company_code, api_code, force_secondary=retry)
 
-    # 发票抬头节点配置
-    ih_node_dict = invoiceHeader_obj.load_api_p_out_nodes(company_code, api_code,
-                                                          node_type=invoiceHeader_obj.BIZ_NODE_LV1)
-    # 拼接发票抬头数据
-    ih_dict = invoiceHeader_obj.splice_data_info(data, node_dict=ih_node_dict)
+    # 节点配置
+    inv_node_dict = Setup.load_api_p_out(company_code, api_code)
+    general_node_dict = inv_node_dict["General"]
+    inv_header_node_dict = {**inv_node_dict[InvoiceHeader.BIZ_NODE_LV1], **inv_node_dict[InvoiceHeader.BIZ_NODE_LV2]}
+    inv_line_node_dict = {**inv_node_dict[InvoiceLine.BIZ_NODE_LV1], **inv_node_dict[InvoiceLine.BIZ_NODE_LV2]}
 
-    if len(ih_dict) > 0:
-        invoiceHeader_obj.save_data_to_nav(nav_data=ih_dict, entry_no=entry_no, TABLE_CLASS=invoiceHeader_obj.TABLE_CLASS)
+    # 读取数据
+    path, data = invh_obj.load_data(api_setup, file_path=file_path)
 
-        # 发票明细节点配置
-        il_node_dict = invoiceLine_obj.load_api_p_out_nodes(company_code, api_code, node_type=invoiceLine_obj.BIZ_NODE_LV1)
-        # 拼接发票明细数据
-        il_dict = invoiceLine_obj.splice_data_info(data, node_dict=il_node_dict)
-        invoiceLine_obj.save_data_to_nav(nav_data=il_dict, entry_no=entry_no, TABLE_CLASS=invoiceLine_obj.TABLE_CLASS)
+    general_dict = invh_obj.splice_general_info(data, node_dict=general_node_dict)
+    count = invh_obj.get_count_from_data(data["Transaction"], InvoiceHeader.BIZ_NODE_LV1)
+    # 发票头
+    invh_dict = invh_obj.splice_data_info(data, node_dict=inv_header_node_dict)
+    # 发票行
+    invl_dict = invl_obj.splice_data_info(data, node_dict=inv_line_node_dict)
+
+    # 写数据
+    entry_no = nav.insertGeneral(api_p_out=general_node_dict, data_dict=general_dict, Type=2, Count=count, XMLFile=path)
+    if len(invh_dict) > 0:
+        # 写发票头数据
+        nav.insertInvHeader(api_p_out=inv_header_node_dict, data_dict=invh_dict, entry_no=entry_no)
+
+        # 写发票行数据
+        nav.insertInvLines(api_p_out=inv_line_node_dict, data_dict=invl_dict, entry_no=entry_no)
 
     # 读取文件，文件归档
-    invoiceLine_obj.archive_xml(xml_src_path, api_setup.Archived_Path)
+    invh_obj.archive_xml(path, api_setup.Archived_Path)
 
     # 读取web service
     wsh = WebServiceHandler(api_setup, soap_username=company_info.NAV_WEB_UserID,
                             soap_password=company_info.NAV_WEB_Password)
     ws_url = wsh.soapAddress(company_info.NAV_Company_Code)
     ws_env = WebServiceHandler.soapEnvelope(entry_no=entry_no, command_code=api_setup.CallBack_Command_Code)
-    wsh.call_web_service(ws_url, ws_env, direction=invoiceHeader_obj.DIRECT_NAV, async_invoke=async_ws,
+    wsh.call_web_service(ws_url, ws_env, direction=invh_obj.DIRECT_NAV, async_invoke=async_ws,
                          soap_action=api_setup.CallBack_SoapAction)
     return entry_no
 
