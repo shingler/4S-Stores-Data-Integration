27eed1f4457b329e3ee4b9188e2e6f06eafe4b07
shingler <singlerwong@qq.com>
2020-12-18 13:19:46 +0800

    提醒邮件发送失败时，记录文件日志；

--- bin/task.py
+++ bin/task.py
@@ -114,12 +114,14 @@ class Handler:
                                                                              self.current_task.API_Code,
                                                                              error_message=self.load_error)
 
-            result = notify_obj.send_mail(email_title, email_content)
-            # print(result)
-            # 写入提醒日志
-            if result:
+            try:
+                notify_obj.send_mail(email_title, email_content)
+                # 发送成功写提醒日志
                 notify_obj.save_notification_log(",".join(notify_obj.receivers), email_title, email_content)
-
+            except Exception as ex:
+                # 发送失败写文件日志
+                self.logger.warning("notification email send failed. %s" % ex)
+                
 
 if __name__ == '__main__':
     # 参数处理
